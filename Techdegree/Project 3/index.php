f8be7594a845e255b6495337ae550fb4d63f0fb9
mdbyrd1920 <53909144+mdbyrd1920@users.noreply.github.com>
2019-11-19 14:47:28 -0500

    Add files via upload

--- Techdegree/Project 3/index.php
+++ Techdegree/Project 3/index.php
@@ -8,21 +8,8 @@ include ("inc/header.php");
             <div class="container">
                 <div class="entry-list">
                     <article>
-                        <h2><a href="detail.php">The best day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_2.php">The absolute worst day I’ve ever had</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_3.php">That time at the mall</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-                    </article>
-                    <article>
-                        <h2><a href="detail_4.php">Dude, where’s my car?</a></h2>
-                        <time datetime="2016-01-31">January 31, 2016</time>
-
+                    <?php  
+                            ?>
                     </article>
                 </div>
             </div>
